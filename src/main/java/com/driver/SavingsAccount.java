487fc2cffe39f9db6de020852da7645806bef2cd
shanawazSDE <doccuekno1@gmail.com>
2023-05-03 17:23:50 +0530

    rectified the savings ac final

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -3,6 +3,7 @@ package com.driver;
 public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
+    private double amountWithdrawn;
 
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
@@ -10,6 +11,7 @@ public class SavingsAccount extends BankAccount{
         super(name,balance,0);
         this.rate = rate;
         this.maxWithdrawalLimit = maxWithdrawalLimit;
+        amountWithdrawn = 0;
 
 
     }
@@ -17,9 +19,9 @@ public class SavingsAccount extends BankAccount{
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-        if(amount > maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
+        if(amount+amountWithdrawn > maxWithdrawalLimit) throw new Exception("Maximum Withdraw Limit Exceed");
         if(amount > getBalance()) throw new Exception("Insufficient Balance");
-
+        amountWithdrawn += amount;
 
         setBalance(getBalance() - amount);
     }
