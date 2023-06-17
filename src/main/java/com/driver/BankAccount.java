8a5d5a5e84029da7afbfe78b771b90ede1f8cf30
ShanawazSDE <shanawaz.sde@gmail.com>
2023-06-17 14:56:54 +0530

    try1

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,31 +1,97 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class BankAccount {
 
     private String name;
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+    private Set<String> currentAccounts;
 
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+        currentAccounts = new HashSet<>();
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if(sum > (9 * digits) || sum < 0) throw new Exception("Account Number can not be generated");
+        List<String> list = new ArrayList<>();
+        generateAllPossibleAccountNumbers(digits,sum,"",list);
 
-        return null;
+        String acc = "";
+        for(String s : list){
+            if(!currentAccounts.contains(s)){
+                acc = s;
+                currentAccounts.add(s);
+                break;
+            }
+        }
+        if (acc.isEmpty()){
+            throw new Exception("Account Number can not be generated");
+        }
+        return acc;
+
     }
+
+    private void generateAllPossibleAccountNumbers(int digits, int sum, String temp, List<String> list) {
+        if(digits == 0){
+            if(sum == 0){
+                list.add(temp);
+            }
+            return;
+        }
+        for(int i = 0; i <= 9; i++){
+            generateAllPossibleAccountNumbers(digits-1,sum-i,temp+i,list);
+        }
+    }
+
+
 
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        double rem = balance - amount;
+        if(rem < minBalance)throw new Exception("Insufficient Balance");
+        else{
+            balance = rem;
+        }
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,18 @@
 package com.driver;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
-
+    Set<String> tradeIdSet ;
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000);
+        this.tradeLicenseId = tradeLicenseId;
+        tradeIdSet = new HashSet<>();
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,6 +20,46 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+       if(!isValid(tradeLicenseId)){
+           List<String> list = getAllPermutations(tradeLicenseId);
+           for(String id : list){
+               if(isValid(id) && !tradeIdSet.contains(id)){
+                   tradeLicenseId = id;
+                   tradeIdSet.add(id);
+               }
+           }
+           if(!isValid(tradeLicenseId)){
+               throw new Exception("Valid License can not be generated");
+           }
+       }
+    }
+    private boolean isValid(String id){
+        int len = id.length();
+
+        for(int i = 1; i < len; i++){
+            if(id.charAt(i) == id.charAt(i-1))return false;
+        }
+        return true;
+    }
+
+
+    private List<String> getAllPermutations(String s){
+        if(s.length() == 1){
+            return List.of(s);
+        }
+        List<String> res = getAllPermutations(s.substring(1));
+        List<String> ans = new ArrayList<>();
+        char c = s.charAt(0);
+
+        for(String str : res){
+            ans.add(c+str);
+            for(int i = 1; i < str.length(); i++){
+                ans.add(str.substring(0,i)+c+str.substring(i));
+            }
+            ans.add(str+c);
+        }
+        return ans;
+
 
     }
 
